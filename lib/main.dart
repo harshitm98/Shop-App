38bdf5a97c64300b0a96d761350eb92b4bacca11
Harshit Maheshwari <harshitmah98@gmail.com>
2019-09-02 23:12:18 +0530

    Implements state management using Providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import './screens/product_detail_screen.dart';
+import './screens/products_overview_screen.dart';
+import './providers/products_provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'My Shop',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider(
+      builder: (ctx) => ProductsProvider(),
+      child: MaterialApp(
+        title: 'My Shop',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: "Lato",
+        ),
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeNamed: (ctx) => ProductDetailScreen(),
+        },
       ),
-      home: MyHomePage(title: 'MyShop'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(""),
       ),
       body: Text("Test"),
     );
---
+++ lib/screens/product_detail_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products_provider.dart';
+
+class ProductDetailScreen extends StatelessWidget {
+  static const routeNamed = "/product-detail-screen";
+
+  @override
+  Widget build(BuildContext context) {
+    final productId = ModalRoute.of(context).settings.arguments as String;
+    
+    final loadedProduct = Provider.of<ProductsProvider>(
+      context,
+      listen: false,  // listen: false - would not re run when the data is changed. Doing this because we only data one time
+    ).findById(productId);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(loadedProduct.title),
+      ),
+    );
+  }
+}
