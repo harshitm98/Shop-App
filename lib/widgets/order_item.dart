d7501e287ba52a3b04f1485eefa24c9a6a769935
Harshit Maheshwari <harshitmah98@gmail.com>
2019-09-13 19:37:28 +0530

    Implements Orders list with drop down

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,13 +1,20 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../providers/orders.dart' as ord;
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final ord.OrderItem order;
 
   OrderItem(this.order);
 
+  @override
+  _OrderItemState createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  var _expanded = false;
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -15,10 +22,43 @@ class OrderItem extends StatelessWidget {
       child: Column(
         children: <Widget>[
           ListTile(
-            title: Text("\$${order.amount}"),
-            subtitle: Text(DateFormat("dd/MM/yyyy hh:mm").format(order.dateTime)),
-            trailing: IconButton(icon: Icon(Icons.expand_more), onPressed: () {},),
+            title: Text("\$${widget.order.amount.toStringAsFixed(2)}"),
+            subtitle: Text(
+                DateFormat("dd/MM/yyyy hh:mm").format(widget.order.dateTime)),
+            trailing: IconButton(
+              icon:
+                  _expanded ? Icon(Icons.expand_less) : Icon(Icons.expand_more),
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
+            ),
           ),
+          if (_expanded)
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+              height: min(widget.order.products.length * 20.0 + 10, 100),
+              child: ListView.builder(
+                itemCount: widget.order.products.length,
+                itemBuilder: (ctx, i) {
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        widget.order.products[i].title,
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "${widget.order.products[i].quantity}x \$${widget.order.products[i].price}",
+                        style: TextStyle(fontSize: 18, color: Colors.grey),
+                      )
+                    ],
+                  );
+                },
+              ),
+            ),
         ],
       ),
     );
